687df20e7c0bc4a23cd8d107a5bb2bd0c156ef9a
Tomáš Chobola <chobola@in.tum.de>
2022-10-13 22:14:34 +0200

    Update rl.py

--- rl.py
+++ rl.py
@@ -17,9 +17,9 @@ def torch_richardson_lucy(image, psf, num_iter=50):
     eps = 1e-12
 
     for _ in range(num_iter):
-        conv = torch.conv2d(im_deconv, psf, stride=1, padding=int(psf.shape[-1]//2)) + eps
+        conv = torch.conv2d(im_deconv, psf, stride=1, padding=psf.shape[-1]//2) + eps
         relative_blur = image / conv
-        im_deconv *= torch.conv2d(relative_blur, psf_mirror, stride=1, padding=int(psf.shape[-1]//2)) + eps    
+        im_deconv *= torch.conv2d(relative_blur, psf_mirror, stride=1, padding=psf.shape[-1]//2) + eps    
         im_deconv = torch.clip(im_deconv, -1, 1)
     
     return im_deconv[:,:,pad:-pad,pad:-pad]
