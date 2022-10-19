b524037c2342a7237dfe58d6ffbaaf376272e2f8
Tomáš Chobola <chobola@in.tum.de>
2022-10-19 09:19:44 +0200

    Gradient step fix

--- landweber.py
+++ landweber.py
@@ -17,7 +17,7 @@ def torch_landweber(image, psf, num_iter=50, lam=0.7):
         conv = torch.conv2d(im_deconv, psf, stride=1, padding=psf.shape[-1]//2)
         res = image - conv
         conv2 = torch.conv2d(res, torch.flip(psf, [0, 1, 2, 3]), stride=1, padding=psf.shape[-1]//2)
-        im_deconv = im_deconv + lam * conv2
+        im_deconv = im_deconv - lam * conv2
         im_deconv = torch.clip(im_deconv, -1, 1)
     
     return im_deconv[:,:,pad:-pad,pad:-pad]
